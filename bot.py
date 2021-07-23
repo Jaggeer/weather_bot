5114f86ecc910aa7a868bb30d544c44c802a2cb0
Jaggeer <speedkirya@gmail.com>
2021-07-24 00:04:49 +0300

    Added few new commands for working with database and finite state machine

--- bot.py
+++ bot.py
@@ -2,35 +2,79 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from config import API_TOKEN
+from database  import Database
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 API = API_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API)
-dp = Dispatcher(bot)
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+db = Database('db.db')
+
+class Form(StatesGroup):
+	city = State()
+	update = State()
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm WeatherBot!\nI'll send you weather in your city.")
+    await message.answer("Привет! Используй комманду /help, чтобы увидеть мой список комманд!")
+
 
 @dp.message_handler(commands=['help'])
 async def command_help(message: types.Message):
-	msg = ("My commands:\n/start - welcome message\n/help - list of commands\n/city - select your city\n/update -"
-			+ " update your city\n/getweather -"
-			+ " get weather in your sity\n")
-	await message.reply(msg)
+	msg = ("Я могу ответить на следующие команды:\n/help - помощь\n/city - выбор города для новых пользователей"
+			+"\n/update - смена города\n/getweather - погода на текущее время")
+	await message.answer(msg)
+
 
 @dp.message_handler(commands=['city'])
 async def command_city(message: types.Message):
+	msg = "Напишите город для отображения погоды"
+	await message.answer(msg)
+	await Form.city.set()
+
+
+@dp.message_handler(state=Form.city)
+async def command_set_city(message: types.Message, state: FSMContext):
+	msg = message.text
+	if(not db.user_exists(message.from_user.id)):
+		db.set_city(message.from_user.id, msg)
+		await message.answer("Вы добавлены в базу данных!")
+		await state.finish()
+	else:
+		await message.answer("Вы уже в базе данных.\nИспользуйте комманду /update, чтобы обновить город.")
+		await state.finish()
 
 
 @dp.message_handler(commands=['update'])
 async def command_update(message: types.Message):
+	msg = "Напишите на какой город вы хотите заменить текущий"
+	await message.answer(msg)
+	await Form.update.set()
 
 
-@dp.message_handler(commands=['getweather'])
-async def command_getweather(message: types.Message):
+@dp.message_handler(state=Form.update)
+async def command_update_city(message: types.Message, state: FSMContext):
+	msg = message.text
+	if(not db.user_exists(message.from_user.id)):
+		db.set_city(message.from_user.id, msg)
+		await message.answer("Вы добавлены в базу данных!")
+		await state.finish()
+	else:
+		db.update_city(message.from_user.id, msg)
+		await message.answer("Вы поменяли город.")
+		await state.finish()
+
+
+"""@dp.message_handler(commands=['getweather'])
+async def command_getweather(message: types.Message):"""
 
 
 if __name__ == '__main__':
